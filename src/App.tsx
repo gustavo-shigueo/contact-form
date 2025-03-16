cecbcba11a5df4019225f7302d65f28ac6397fdb
Gustavo Shigueo <gustavo.gsmn@gmail.com>
2025-03-16 19:11:28 -0300

    Initial HTML markup

--- src/App.tsx
+++ src/App.tsx
@@ -3,33 +3,84 @@ import type { Component } from 'solid-js';
 const App: Component = () => {
   return (
     <>
-      Contact Us
+      <main>
+        <h1>Contact Us</h1>
 
-      First Name
-      This field is required
+        <form id="form">
+          <div class="field">
+            <label for="first-name">First Name</label>
+            <input id="first-name" name="firstName" type="text" required autocomplete="given-name" />
 
-      Last Name
-      This field is required
+            <ul class="errors">
+              <li><span aria-live="polite">This field is required</span></li>
+            </ul>
+          </div>
 
-      Email Address
-      Please enter a valid email address
-      This field is required
+          <div class="field">
+            <label for="last-name">Last Name</label>
+            <input id="last-name" name="lastName" type="text" required autocomplete="family-name" />
 
-      Query Type
-      General Enquiry
-      Support Request
-      Please select a query type
+            <ul class="errors">
+              <li><span aria-live="polite">This field is required</span></li>
+            </ul>
+          </div>
 
-      Message
-      This field is required
+          <div class="field">
+            <label for="email">Email Address</label>
+            <input id="email" name="email" type="email" required autocomplete="email"/>
 
-      I consent to being contacted by the team
-      To submit this form, please consent to being contacted
+            <ul class="errors">
+              <li><span aria-live="polite">Please enter a valid email address</span></li>
+              <li><span aria-live="polite">This field is required</span></li>
+            </ul>
+          </div>
 
-      Submit
+          <div class="field">
+            <fieldset>
+              <legend>Query Type</legend>
 
-      Message Sent!
-      Thanks for completing the form. We'll be in touch soon!
+              <div class="radio-button">
+                <input id="general-enquiry" name="query-type" type="radio" value="general-enquiry" required />
+                <label for="general-enquiry">General Enquiry</label>
+              </div>
+
+              <div class="radio-button">
+                <input id="support-request" name="query-type" type="radio" value="support-request" required />
+                <label for="support-request">Support Request</label>
+              </div>
+
+              <ul class="errors">
+                <li><span aria-live="polite">Please select a query type</span></li>
+              </ul>
+            </fieldset>
+          </div>
+
+          <div class="field">
+            <label for="message">Message</label>
+            <textarea id="message" name="message" required />
+
+            <ul class="errors">
+              <li><span aria-live="polite">This field is required</span></li>
+            </ul>
+          </div>
+
+          <div class="field">
+            <input id="consent" name="consent" type="checkbox" required />
+            <label for="consent">I consent to being contacted by the team</label>
+            
+            <ul class="errors">
+              <li><span aria-live="polite">To submit this form, please consent to being contacted</span></li>
+            </ul>
+          </div>
+
+          <button type="submit">Submit</button>
+        </form>
+      </main>
+
+      <output form="form">
+        <strong>Message Sent!</strong>
+        <p>Thanks for completing the form. We'll be in touch soon!</p>
+      </output>
       
       <div class="attribution">
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>. 
